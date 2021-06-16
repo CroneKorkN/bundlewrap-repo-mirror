694fe3f6338206d5bbdebfb22bb4e2f605d9ce42
mwiegand <mwiegand@seibert-media.net>
2021-06-16 22:28:32 +0200

    wip

---
+++ bundles/rspamd/items.py
@@ -0,0 +1,66 @@
+from os import listdir
+from os.path import join
+
+repo.libs.tools.require_bundle(node, 'redis', 'rspamd does not work without a redis cache')
+
+directories = {
+    '/etc/rspamd/local.d': {
+        'purge': True,
+        'needs': {
+            'pkg_apt:rspamd',
+        },
+        'triggers': {
+            'svc_systemd:rspamd:restart',
+        },
+    },
+    '/etc/rspamd/override.d': {
+        'purge': True,
+        'needs': {
+            'pkg_apt:rspamd',
+        },
+        'triggers': {
+            'svc_systemd:rspamd:restart',
+        },
+    },
+}
+
+files = {
+    '/etc/rspamd/local.d/ip_whitelist.map': {
+        'content_type': 'mako',
+        'triggers': {
+            'svc_systemd:rspamd:restart',
+        },
+    },
+    '/etc/rspamd/local.d/worker-controller.inc': {
+        'content_type': 'mako',
+        'triggers': {
+            'svc_systemd:rspamd:restart',
+        },
+    }
+}
+
+local_config_path = join(repo.path, 'bundles', 'rspamd', 'files', 'local.d')
+for f in listdir(local_config_path):
+    files[f'/etc/rspamd/local.d/{f}'] = {
+        'source': f'local.d/{f}',
+        'triggers': {
+            'svc_systemd:rspamd:restart',
+        },
+    }
+
+override_config_path = join(repo.path, 'bundles', 'rspamd', 'files', 'override.d')
+for f in listdir(override_config_path):
+    files[f'/etc/rspamd/override.d/{f}'] = {
+        'source': f'override.d/{f}',
+        'triggers': {
+            'svc_systemd:rspamd:restart',
+        },
+    }
+
+svc_systemd = {
+    'rspamd': {
+        'needs': {
+            'pkg_apt:rspamd',
+        },
+    },
+}
--- bundles/rspamd/metadata.py
+++ bundles/rspamd/metadata.py
@@ -1,7 +1,15 @@
 defaults = {
     'apt': {
         'packages': {
+            'clamav': {},
+            'clamav-daemon': {},
+            'clamav-freshclam': {},
+            'clamav-unofficial-sigs': {}, 
             'rspamd': {},
         },
     },
-} 
+    'rspamd': {
+        'web_password': repo.vault.password_for(node.name + ' rspamd web password'),
+        'ignore_spam_check_for_ips': [],
+    },
+}
