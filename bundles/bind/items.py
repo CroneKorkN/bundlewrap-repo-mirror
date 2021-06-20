946b6c439a2597e839e1c6881e30a769c5e5a0dd
mwiegand <mwiegand@seibert-media.net>
2021-06-20 11:57:30 +0200

    wip

--- bundles/bind/items.py
+++ bundles/bind/items.py
@@ -6,15 +6,11 @@ files['/etc/default/bind9'] = {
     'source': 'defaults',
 }
 
-def column_width(column, table):
-    return max(map(lambda row: len(row[column]), table)) if table else 0
-
 for zone, records in node.metadata.get('bind/zones').items():
     files[f'/var/lib/bind/db.{zone}'] = {
         'source': 'db',
         'content_type': 'mako',
         'context': {
             'records': records,
-            'column_width': column_width,
         }
     }
