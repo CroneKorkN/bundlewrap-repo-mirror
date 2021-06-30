e799cf8c6c538aafe23d4294d2fb7b918f0e23b1
mwiegand <mwiegand@seibert-media.net>
2021-06-30 02:51:23 +0200

    wip

--- libs/ini.py
+++ libs/ini.py
@@ -1,17 +1,23 @@
 from configparser import ConfigParser
 
+def parse(text):
+    config = ConfigParser()
+    config.read_string(text)
+    
+    return {
+        section: dict(config.items(section))
+            for section in config.sections()
+    }
+
 class Writable():
     data = ''
-    
+
     def write(self, line):
         self.data += line
 
 def dumps(dict):
     config = ConfigParser()
-    
-    for section, settings in dict.items():
-        config[section] = settings
-
+    config.read_dict(dict)
     writable = Writable()
     config.write(writable)
 
