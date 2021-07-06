c410be5d728a69c8c77dd36f1d3f84eb4d2987ef
mwiegand <mwiegand@seibert-media.net>
2021-07-06 21:32:38 +0200

    wip

--- bundles/backup-server/metadata.py
+++ bundles/backup-server/metadata.py
@@ -10,50 +10,6 @@ defaults = {
 
 
 @metadata_reactor.provides(
-    'zfs/datasets'
-)
-def zfs(metadata):
-    datasets = {}
-    
-    for other_node in repo.nodes:
-        if (
-            other_node.has_bundle('backup') and
-            other_node.metadata.get('backup/server') == node.name
-        ):
-            datasets[f"tank/{other_node.metadata.get('id')}/fs"] = {
-                'mountpoint': f"/mnt/backups/{other_node.metadata.get('id')}",
-                'backup': False,
-            }
-            
-            if other_node.has_bundle('zfs'):
-                for path in other_node.metadata.get('backup/paths'):
-                    for dataset, config in other_node.metadata.get('zfs/datasets').items():
-                        if path == config.get('mountpoint'):
-                            datasets[f"tank/{other_node.metadata.get('id')}/{dataset}"] = {
-                                'mountpoint': 'none',
-                                'backup': False,
-                            }
-
-
-    return {
-        'zfs': {
-            'datasets': datasets,
-        },
-    }
-
-
-@metadata_reactor.provides(
-    'dns',
-)
-def dns(metadata):
-    return {
-        'dns': {
-            metadata.get('backup-server/hostname'): repo.libs.dns.get_a_records(metadata),
-        }
-    }
-
-
-@metadata_reactor.provides(
     'users/backup-receiver/authorized_keys'
 )
 def backup_authorized_keys(metadata):
