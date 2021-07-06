a962ce32a265f73b037d51520ecc36e7b5165a04
mwiegand <mwiegand@seibert-media.net>
2021-07-06 21:38:21 +0200

    wip

--- bundles/users/metadata.py
+++ bundles/users/metadata.py
@@ -11,24 +11,9 @@ def user(metadata):
     for name, config in metadata.get('users').items():
         users[name] = {
             'authorized_keys': [],
+            'privkey': '111',
+            'privkey': 'pubkey',
         }
-
-        if not 'full_name' in config:
-            users[name]['full_name'] = name
-
-        if not 'home' in config:
-            users[name]['home'] = f'/home/{name}'
-
-        if not 'shell' in config:
-            users[name]['shell'] = '/bin/bash'
-            
-        if not 'privkey' in users[name] and not 'pubkey' in users[name]:
-            privkey, pubkey = repo.libs.ssh.generate_ed25519_key_pair(
-                b64decode(str(repo.vault.random_bytes_as_base64_for(f"{name}@{node.name}", length=32)))
-            )
-            users[name]['keytype'] = 'ed25519'
-            users[name]['privkey'] = privkey
-            users[name]['pubkey'] = pubkey + f' {name}@{node.name}'
     
     return {
         'users': users,
--- groups/all.py
+++ groups/all.py
@@ -7,12 +7,7 @@
             'server': 'home.backups',
         },
         'users': {
-            'root': {
-                'shell': '/usr/bin/zsh',
-                'authorized_keys': [
-                    'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEU1l2ijW3ZqzFGZcdWg2ESgTGehdNfBTfafxsjWvWdS mwiegand@macbook',
-                ],
-            },
+            'root': {},
         },
     }
 }
