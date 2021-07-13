6e5b6c0f0554f5b20b04bbe31ca07928079c48e7
mwiegand <mwiegand@seibert-media.net>
2021-07-13 15:29:44 +0200

    wip

--- bundles/zfs/metadata.py
+++ bundles/zfs/metadata.py
@@ -7,28 +7,5 @@ defaults = {
             'when': 'Sat 00:00',
             'persistent': True,
         },
-        'zfs-scrub': {
-            'command': '/usr/lib/zfs-linux/scrub',
-            'when': 'Sun 00:00',
-            'persistent': True,
-        },
-        'zfs-auto-snapshot-hourly': {
-            'command': '/usr/sbin/zfs-auto-snapshot --quiet --syslog --label=hourly --keep=24 //',
-            'when': 'hourly',
-        },
-        'zfs-auto-snapshot-daily': {
-            'command': '/usr/sbin/zfs-auto-snapshot --quiet --syslog --label=daily --keep=7 //',
-            'when': 'daily',
-        },
-        'zfs-auto-snapshot-weekly': {
-            'command': '/usr/sbin/zfs-auto-snapshot --quiet --syslog --label=weekly --keep=4 //',
-            'when': 'weekly',
-            'persistent': True,
-        },
-        'zfs-auto-snapshot-monthly': {
-            'command': '/usr/sbin/zfs-auto-snapshot --quiet --syslog --label=monthly --keep=24 //',
-            'when': 'monthly',
-            'persistent': True,
-        },
     },
 }
