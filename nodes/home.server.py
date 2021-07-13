92d3b7ac834366d1599c648ec4bdf631f1344a96
mwiegand <mwiegand@seibert-media.net>
2021-07-13 15:23:13 +0200

    wip

--- nodes/home.server.py
+++ nodes/home.server.py
@@ -1,30 +1,13 @@
 {
     'hostname': '10.0.0.2',
-    'groups': [
-#        'archive',
-        'debian-10',
-    ],
     'bundles': [
+        'systemd',
+        'systemd-timers',
         'zfs',
     ],
     'metadata': {
+        'os_release': 'buster',
         'id': 'af96709e-b13f-4965-a588-ef2cd476437a',
-        'network': {
-            'internal': {
-                'interface': 'enp1s0f0',
-                'ipv4': '10.0.0.2/24',
-                'gateway4': '10.0.0.1',
-            },
-        },
-        'users': {
-            'root': {
-                'shell': '/usr/bin/zsh',
-            },
-        },
-        'vm': {
-            'cores': 2,
-            'ram':  16192,
-        },
         'zfs': {
             'pools': {
                 'tank': {
@@ -36,4 +19,7 @@
             },
         },
     },
+    'os_version': (10,),
+    'os': 'debian',
+    'pip_command': 'pip3',
 }
