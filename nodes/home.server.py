198c1f54faaa4118a97ad4366e38d1be6026622c
mwiegand <mwiegand@seibert-media.net>
2021-07-13 15:29:28 +0200

    wip

--- nodes/home.server.py
+++ nodes/home.server.py
@@ -7,17 +7,6 @@
     ],
     'metadata': {
         'os_release': 'buster',
-        'id': 'af96709e-b13f-4965-a588-ef2cd476437a',
-        'zfs': {
-            'pools': {
-                'tank': {
-                    'mirrors': [
-                        '/dev/disk/by-partlabel/zfs-data-1',
-                        '/dev/disk/by-partlabel/zfs-data-2',
-                    ],
-                },
-            },
-        },
     },
     'os_version': (10,),
     'os': 'debian',
