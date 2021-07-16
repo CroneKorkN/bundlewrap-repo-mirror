e859467b0cf889b5847a7ec6fa863852d2088d82
mwiegand <mwiegand@seibert-media.net>
2021-07-17 01:16:23 +0200

    wip

--- bundles/rspamd/metadata.py
+++ bundles/rspamd/metadata.py
@@ -8,6 +8,16 @@ defaults = {
             'rspamd': {},
         },
     },
+    'nginx': {
+        'vhosts': {
+            'rspamd.sublimity.de': {
+                'content': 'nginx/proxy_pass.conf',
+                'context': {
+                    'target': 'http://localhost:11334',
+                },
+            },
+        },
+    },
     'rspamd': {
         'web_password': repo.vault.password_for(node.name + ' rspamd web password'),
         'ignore_spam_check_for_ips': [],
