cc58a4e74f04f3086815cff97742b842ef2fffb1
mwiegand <mwiegand@seibert-media.net>
2021-08-13 01:15:21 +0200

    wip

--- bundles/systemd-timers/items.py
+++ bundles/systemd-timers/items.py
@@ -1,27 +1,3 @@
-# # svc_systemd['cron'] = {
-# #     'enabled': False,
-# # }
-# 
-# for name, config in node.metadata.get('systemd-timers').items():
-#     files[f'/etc/systemd/system/{name}.timer'] = {
-#         'content': repo.libs.systemd.generate_unitfile({
-#             'Unit':{
-#                 'Description': f'{name} timer',
-#             },
-#             'Timer': {
-#                 'OnCalendar': config['when'],
-#                 'Persistent': config.get('persistent', False),
-#                 'Unit': f'{name}.service',
-#             },
-#             'Install': {
-#                 'WantedBy': 'multi-user.target',
-#             }
-#         }),
-#         'triggers': [
-#             'action:systemd-reload',
-#             f'svc_systemd:{name}:restart',
-#         ],
-#     }
-# 
-#     svc_systemd[f'{name}.timer'] = {}
-# #
+svc_systemd['cron'] = {
+    'enabled': False,
+}
