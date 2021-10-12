0b8b769b12ce43b95d3cf3ebb476b1c81a6f0d5c
mwiegand <mwiegand@seibert-media.net>
2021-10-13 01:34:19 +0200

    wip

--- groups/hardware/raspberry-pi.py
+++ groups/hardware/raspberry-pi.py
@@ -4,7 +4,7 @@
             'config': {
                 'inputs': {
                     'exec': [{
-                        'commands': ["/bin/bash -c \"cat /sys/class/thermal/thermal_zone0/temp | xargs -I '{}' expr {} / 1000\""],
+                        'commands': ["/bin/bash -c 'expr $(cat /sys/class/thermal/thermal_zone0/temp) / 1000'"],
                         'name_override': "cpu_temperature",
                         'data_format': "value",
                         'data_type': "integer",
