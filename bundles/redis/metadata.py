c6c9dc37f70638e97df4ea9f3ee5ce34c908b615
mwiegand <mwiegand@seibert-media.net>
2021-10-18 23:36:16 +0200

    backup redis

--- bundles/redis/metadata.py
+++ bundles/redis/metadata.py
@@ -4,4 +4,21 @@ defaults = {
             'redis-server': {},
         },
     },
-} 
+    'backup': {
+        'paths': {
+            '/var/lib/redis',
+        },
+    }
+}
+
+if node.has_bundle('zfs'):
+    defaults['zfs'] = {
+        'datasets': {
+            'tank/redis': {
+                'mountpoint': '/var/lib/redis',
+                'needed_by': [
+                    'pkg_apt:redis-server',
+                ],
+            },
+        },
+    }
