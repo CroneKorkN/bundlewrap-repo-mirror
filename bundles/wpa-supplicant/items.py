9e4163d291ec663e9ac490311f63d428cb3fc5f1
mwiegand <mwiegand@seibert-media.net>
2021-10-28 23:02:40 +0200

    wpa_supplicant

--- bundles/wpa-supplicant/items.py
+++ bundles/wpa-supplicant/items.py
@@ -1,24 +1,15 @@
 import hashlib, binascii
 
 
-def wpa_psk(ssid, password):
-    return binascii.hexlify(
-        hashlib.pbkdf2_hmac('sha1', str.encode(password), str.encode(ssid), 4096, 32)
-    ).decode()
-    
-
 interface = node.metadata.get('wpa-supplicant/interface')
-ssid = node.metadata.get('wpa-supplicant/ssid')
-passowrd = repo.vault.decrypt(node.metadata.get('wpa-supplicant/password')).value
-psk = wpa_psk(ssid, passowrd)
 
 files = {
     f'/etc/wpa_supplicant/wpa_supplicant-{interface}.conf': {
         'source': 'wpa_supplicant.conf',
         'content_type': 'mako',
         'context': {
-            'ssid': ssid,
-            'psk': psk,
+            'ssid': node.metadata.get('wpa-supplicant/ssid'),
+            'psk': node.metadata.get('wpa-supplicant/psk'),
         },
         'needs': [
             'pkg_apt:wpasupplicant',
