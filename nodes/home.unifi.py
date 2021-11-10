f0ecf64938474a02f10851e0477b847e7aaebc2c
mwiegand <mwiegand@seibert-media.net>
2021-11-10 04:04:14 +0100

    unifi

---
+++ nodes/home.unifi.py
@@ -0,0 +1,16 @@
+{
+    'dummy': True,
+    'metadata': {
+        'id': '',
+        'network': {
+            'internal': {
+                'ipv4': '10.0.0.4/24',
+            },
+        },
+        'dns': {
+            'unifi.ckn.li': {
+                'A': {'10.0.0.4'},
+            },
+        },
+    },
+}
