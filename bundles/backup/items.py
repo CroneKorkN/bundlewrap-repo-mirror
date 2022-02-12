4e56ba6da09e00b57e2a86649e5fc17c5828ca36
mwiegand <mwiegand@seibert-media.net>
2022-02-12 18:42:27 +0100

    wol on backup

--- bundles/backup/items.py
+++ bundles/backup/items.py
@@ -1,9 +1,16 @@
 from json import dumps
 
+
+backup_node = repo.get_node(node.metadata.get('backup/server'))
+
 directories['/opt/backup'] = {}
 
 files['/opt/backup/backup_all'] = {
     'mode': '700',
+    'content_type': 'mako',
+    'context': {
+        'wol_command': backup_node.metadata.get('wol-sleeper/wake_command', False),
+    },
 }
 files['/opt/backup/backup_path'] = {
     'mode': '700',
@@ -20,7 +27,7 @@ directories['/etc/backup'] = {}
 files['/etc/backup/config.json'] = {
     'content': dumps(
         {
-            'server_hostname': repo.get_node(node.metadata.get('backup/server')).metadata.get('backup-server/hostname'),
+            'server_hostname': backup_node.metadata.get('backup-server/hostname'),
             'client_uuid': node.metadata.get('id'),
             'paths': sorted(set(node.metadata.get('backup/paths'))),
         },
