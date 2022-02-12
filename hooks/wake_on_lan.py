386d7bab9b1d3dcf8630f06fbd4f3792fbe53109
mwiegand <mwiegand@seibert-media.net>
2022-02-12 21:01:13 +0100

    wake info

--- hooks/wake_on_lan.py
+++ hooks/wake_on_lan.py
@@ -1,7 +1,14 @@
 from bundlewrap.operations import run_local
+from bundlewrap.utils.ui import io
+from bundlewrap.utils.text import yellow, bold
+
 
 def node_apply_start(repo, node, interactive=False, **kwargs):
     if node.has_bundle('wol-sleeper'):
+        io.stdout('{x} {node}  waking up...'.format(
+            x=yellow('!'),
+            node=bold(node.name)
+        ))
         repo\
             .get_node(node.metadata.get('wol-sleeper/waker'))\
             .run(node.metadata.get('wol-sleeper/wake_command'))
