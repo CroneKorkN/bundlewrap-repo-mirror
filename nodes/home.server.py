faf78e37669b077043991c610a383889791e2836
mwiegand <mwiegand@seibert-media.net>
2022-02-23 18:19:58 +0100

    upgarde nextcloud

--- nodes/home.server.py
+++ nodes/home.server.py
@@ -81,7 +81,7 @@
         },
         'nextcloud': {
             'hostname': 'cloud.sublimity.de',
-            'version': '23.0.0',
+            'version': '23.0.2',
         },
         'nextcloud-picsort': {
             'ckn': {
