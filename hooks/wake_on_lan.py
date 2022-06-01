76a568e8b68e28b5576dd62ce2e8fd765a66dd4d
mwiegand <mwiegand@seibert-media.net>
2022-06-01 11:11:14 +0200

    node run wol

--- hooks/wake_on_lan.py
+++ hooks/wake_on_lan.py
@@ -1,2 +1,5 @@
-def node_apply_start(repo, node, interactive=False, **kwargs):
+def node_apply_start(repo, node, **kwargs):
+    repo.libs.wol.wake(node)
+
+def node_run_start(repo, node, cmd, **kwargs):
     repo.libs.wol.wake(node)
