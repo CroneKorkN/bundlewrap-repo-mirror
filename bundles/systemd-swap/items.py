fbee9a32df079604c4046cbe21383845997a2f37
mwiegand <mwiegand@seibert-media.net>
2022-07-07 15:45:28 +0200

    bundle systemd-swap improvements

--- bundles/systemd-swap/items.py
+++ bundles/systemd-swap/items.py
@@ -4,14 +4,23 @@ assert isinstance(size, int)
 actions = {
     'stop_swap': {
         'command': f'systemctl stop swapfile.swap',
-        'unless': f'rm /swapfile',
+        'unless': f'! systemctl is-active swapfile.swap',
         'triggered': True,
+    },
+    'remove_swapfile': {
+        'command': f'rm /swapfile',
+        'unless': f'! test -e /swapfile',
+        'triggered': True,
+        'needs': {
+            'action:stop_swap',
+        },
     },
     'create_swapfile': {
         'command': f'dd if=/dev/zero of=/swapfile bs={size} count=1',
         'unless': f'stat -c %s /swapfile | grep ^{size}$',
         'preceded_by': {
             'action:stop_swap',
+            'action:remove_swapfile',
         },
         'triggers': {
             'action:initialize_swapfile',
