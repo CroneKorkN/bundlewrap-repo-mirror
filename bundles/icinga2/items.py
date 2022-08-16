a9c0e91ea84f87469f933d7c6e2c96338ad11ad3
mwiegand <mwiegand@seibert-media.net>
2022-08-16 23:22:47 +0200

    wip

--- bundles/icinga2/items.py
+++ bundles/icinga2/items.py
@@ -1,4 +1,4 @@
-# Git-Hash for Icinga1: b63bb0ef52bf213715e567c81e3ed097024e61af
+from ipaddress import ip_interface
 
 directories = {
     '/etc/icinga2': {
@@ -11,7 +11,7 @@ directories = {
         ],
     },
     '/etc/icinga2/conf.d': {
-#        'purge': True,
+        'purge': True,
         'owner': 'nagios',
         'group': 'nagios',
         'mode': '0750',
@@ -93,10 +93,66 @@ files = {
             'svc_systemd:icinga2.service:restart',
         ],
     },
-    # '/etc/icinga2/conf.d/templates.conf': {
-    #     'source': 'conf.d/templates.conf',
-    #     'owner': 'nagios',
-    # },
+    '/etc/icinga2/conf.d/app.conf': {
+        'source': 'conf.d/app.conf',
+        'content_type': 'mako',
+        'owner': 'nagios',
+        'group': 'nagios',
+        'mode': '0640',
+        'triggers': [
+            'svc_systemd:icinga2.service:restart',
+        ],
+    },
+    '/etc/icinga2/conf.d/commands.conf': {
+        'source': 'conf.d/commands.conf',
+        'content_type': 'mako',
+        'owner': 'nagios',
+        'group': 'nagios',
+        'mode': '0640',
+        'triggers': [
+            'svc_systemd:icinga2.service:restart',
+        ],
+    },
+    '/etc/icinga2/conf.d/groups.conf': {
+        'source': 'conf.d/groups.conf',
+        'content_type': 'mako',
+        'owner': 'nagios',
+        'group': 'nagios',
+        'mode': '0640',
+        'triggers': [
+            'svc_systemd:icinga2.service:restart',
+        ],
+    },
+    '/etc/icinga2/conf.d/notifications.conf': {
+        'source': 'conf.d/notifications.conf',
+        'content_type': 'mako',
+        'owner': 'nagios',
+        'group': 'nagios',
+        'mode': '0640',
+        'triggers': [
+            'svc_systemd:icinga2.service:restart',
+        ],
+    },
+    '/etc/icinga2/conf.d/templates.conf': {
+        'source': 'conf.d/templates.conf',
+        'content_type': 'mako',
+        'owner': 'nagios',
+        'group': 'nagios',
+        'mode': '0640',
+        'triggers': [
+            'svc_systemd:icinga2.service:restart',
+        ],
+    },
+    '/etc/icinga2/conf.d/timeperiods.conf': {
+        'source': 'conf.d/timeperiods.conf',
+        'content_type': 'mako',
+        'owner': 'nagios',
+        'group': 'nagios',
+        'mode': '0640',
+        'triggers': [
+            'svc_systemd:icinga2.service:restart',
+        ],
+    },
     '/etc/icinga2/features.d/ido-pgsql.conf': {
         'source': 'features/ido-pgsql.conf',
         'content_type': 'mako',
@@ -139,15 +195,23 @@ files = {
 }
 
 for other_node in repo.nodes:
+    if other_node.dummy:
+        continue
+
     files[f'/etc/icinga2/hosts.d/{other_node.name}.conf'] = {
         'content_type': 'mako',
         'source': 'hosts.d/host.conf',
         'owner': 'nagios',
         'context': {
             'host_name': other_node.name,
-            'host_settings': {},
+            'host_settings': {
+                'address': str(ip_interface(other_node.metadata.get('network/internal/ipv4', None) or other_node.metadata.get('wireguard/my_ip')).ip),
+            },
             'services': other_node.metadata.get('monitoring', {}),
         },
+        'triggers': [
+            'svc_systemd:icinga2.service:restart',
+        ],
     }
 
 svc_systemd = {
