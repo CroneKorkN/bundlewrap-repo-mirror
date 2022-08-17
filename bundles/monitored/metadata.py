f3c5da7404be475e496373dea910c9795fc3c255
mwiegand <mwiegand@seibert-media.net>
2022-08-18 01:50:21 +0200

    wip

--- bundles/monitored/metadata.py
+++ bundles/monitored/metadata.py
@@ -3,12 +3,29 @@ defaults = {
         'services': {
             'test': {
                 'vars.command': '/bin/ls /',
-                'check_command': 'sshmon',
                 'host_name': node.name,
             },
         },
     },
 }
+
+
+@metadata_reactor.provides(
+    'monitoring/services',
+)
+def service_defaults(metadata):
+    return {
+        'monitoring': {
+            'services': {
+                name: {
+                    'check_command': 'sshmon',
+                }
+                    for name, conf in metadata.get('monitoring/services').items()
+                    if 'check_command' not in conf
+            },
+        },
+    }
+
 
 @metadata_reactor.provides(
     'users/sshmon/authorized_users'
