4e25dc000c3aa13a684034147cfe861c4a823f90
mwiegand <mwiegand@seibert-media.net>
2022-08-31 12:30:09 +0200

    change ssh key 2

--- groups/os/linux.py
+++ groups/os/linux.py
@@ -34,7 +34,7 @@
             'ckn': {
                 'shell': '/usr/bin/zsh',
                 'authorized_keys': {
-                    'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEU1l2ijW3ZqzFGZcdWg2ESgTGehdNfBTfafxsjWvWdS mwiegand@macbook',
+                    'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAi6tA8JaeovsxyvNNY0g5OEfErHThJHAOWVLguqvVve mwiegand@macbook',
                 },
             },
         },
