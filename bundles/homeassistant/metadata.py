edebd1588f45f62ea6b0e73f55176b90a39e9237
mwiegand <mwiegand@seibert-media.net>
2022-09-05 13:50:10 +0200

    some mroe homassistant deps

--- bundles/homeassistant/metadata.py
+++ bundles/homeassistant/metadata.py
@@ -1,9 +1,20 @@
 defaults = {
     'apt': {
         'packages': {
+            'python3': {},
             'python3-dev': {},
             'python3-pip': {},
             'python3-venv': {},
+            'libffi-dev': {},
+            'libssl-dev': {},
+            'libjpeg-dev': {},
+            'zlib1g-dev': {},
+            'autoconf': {},
+            'build-essential': {},
+            'libopenjp2-7': {},
+            'libtiff5': {},
+            'libturbojpeg0-dev': {},
+            'tzdata': {},
         },
     },
 }
