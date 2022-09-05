7582e8d9cce4d428e477b7b1a340b1ca1f332f89
mwiegand <mwiegand@seibert-media.net>
2022-09-05 15:01:49 +0200

    build server htt check path

--- bundles/build-server/metadata.py
+++ bundles/build-server/metadata.py
@@ -71,6 +71,7 @@ def nginx(metadata):
                     'context': {
                         'target': 'http://127.0.0.1:4000',
                     },
+                    'check_path': '/status',
                 },
             },
         },
