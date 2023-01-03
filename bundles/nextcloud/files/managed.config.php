edf7ab423610a3b8541d0964a59e0b1b35687947
cronekorkn <git@ckn.li>
2023-01-03 15:10:37 +0100

    bundles/nextcloud/files/managed.config.php: decode to assoc array instead of object

--- bundles/nextcloud/files/managed.config.php
+++ bundles/nextcloud/files/managed.config.php
@@ -1,3 +1,3 @@
 <?php
 # https://docs.nextcloud.com/server/stable/admin_manual/configuration_server/config_sample_php_parameters.html#multiple-config-php-file
-$CONFIG = json_decode(file_get_contents("/etc/nextcloud/managed.config.json"));
+$CONFIG = json_decode(file_get_contents("/etc/nextcloud/managed.config.json"), $associative = true);
