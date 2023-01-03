fca2cf0a643aa450c98aa467481894b3246a4a34
cronekorkn <git@ckn.li>
2023-01-03 14:52:11 +0100

    wip

---
+++ bundles/mastodon/metadata.py
@@ -0,0 +1,39 @@
+assert node.has_bundle('postgresql')
+assert node.has_bundle('redis')
+
+defaults = {
+    'apt': {
+        'packages': {
+            # 'imagemagick': {},
+            # 'ffmpeg': {},
+            # 'libpq-dev': {},
+            # 'libxml2-dev': {},
+            # 'libxslt1-dev': {},
+            # 'file': {},
+            # 'git-core': {},
+            # 'g++': {},
+            # 'libprotobuf-dev': {},
+            # 'protobuf-compiler': {},
+            # 'pkg-config': {},
+            # 'nodejs': {},
+            # 'gcc': {},
+            # 'autoconf': {},
+            # 'bison': {},
+            # 'build-essential': {},
+            # 'libssl-dev': {},
+            # 'libyaml-dev': {},
+            # 'libreadline6-dev': {},
+            # 'zlib1g-dev': {},
+            # 'libncurses5-dev': {},
+            # 'libffi-dev': {},
+            # 'libgdbm-dev': {},
+            # 'nginx': {},
+            # 'libidn11-dev': {},
+            # 'libicu-dev': {},
+            # 'libjemalloc-dev': {},
+        },
+    },
+}
+
+
+
