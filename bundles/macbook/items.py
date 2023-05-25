0fb18993227b03aa68df9fb1c906bcac24cd72ca
cronekorkn <git@ckn.li>
2023-05-25 10:57:39 +0200

    macbook more managed

--- bundles/macbook/items.py
+++ bundles/macbook/items.py
@@ -1,8 +1,25 @@
 directories['/Users/mwiegand/.config/bundlewrap/lock'] = {}
 
+# home
+
+files['/Users/mwiegand/.zshrc'] = {
+    'source': 'zshrc',
+    'mode': '0644',
+}
+
+# updater
+
 files['/Users/mwiegand/.bin/macbook-update'] = {
     'mode': '755',
 }
+
+# direnv
+
+directories['/Users/mwiegand/.local/share/direnv'] = {}
+files['/Users/mwiegand/.local/share/direnv/gnu'] = {}
+files['/Users/mwiegand/.local/share/direnv/pyenv'] = {}
+files['/Users/mwiegand/.local/share/direnv/venv'] = {}
+files['/Users/mwiegand/.local/share/direnv/bundlewrap'] = {}
 
 ##################
 
