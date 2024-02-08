6602f2569a7d8fec223b62851270da1e436871b5
cronekorkn <git@ckn.li>
2024-02-08 16:15:57 +0100

    wip

--- bundles/freescout/items.py
+++ bundles/freescout/items.py
@@ -1,4 +1,7 @@
 # https://github.com/freescout-helpdesk/freescout/wiki/Installation-Guide
+run_as = repo.libs.tools.run_as
+php_version = node.metadata.get('php/version')
+
 
 directories = {
     '/opt/freescout': {
@@ -8,13 +11,45 @@ directories = {
     },
 }
 
-git_deploy = {
-    '/opt/freescout': {
-        'repo': 'https://github.com/freescout-helpdesk/freescout.git',
-        'rev': 'master',
+actions = {
+    'clone_freescout': {
+        'command': run_as('www-data', 'git clone https://github.com/freescout-helpdesk/freescout.git /opt/freescout'),
+        'unless': 'test -e /opt/freescout/.git',
+        'needs': [
+            'pkg_apt:git',
+            'directory:/opt/freescout',
+        ],
+    },
+    'pull_freescout': {
+        'command': run_as('www-data', 'git -C /opt/freescout pull'),
+        'unless': run_as('www-data', 'git -C /opt/freescout fetch origin && git -C /opt/freescout status -uno | grep -q "Your branch is up to date"'),
+        'needs': [
+            'action:clone_freescout',
+        ],
+        'triggers': [
+            'action:freescout_artisan_update',
+            f'svc_systemd:php{php_version}-fpm.service:restart',
+        ],
+    },
+    'freescout_artisan_update': {
+        'command': run_as('www-data', 'php /opt/freescout/artisan freescout:after-app-update'),
+        'triggered': True,
+        'needs': [
+            f'svc_systemd:php{php_version}-fpm.service:restart',
+            'action:pull_freescout',
+        ],
     },
 }
 
-
 files = {
+    '/opt/freescout/.env': {
+        'content': '\n'.join(
+            f'{k}={v}' for k, v in
+                sorted(node.metadata.get('freescout/env').items())
+        ),
+        'needs': [
+            'directory:/opt/freescout',
+            'action:clone_freescout',
+        ],
+    },
 }
