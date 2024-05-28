686fa86c3f2f1e1f6e1c4e56cc5dc46d3f3ec0f6
cronekorkn <git@ckn.li>
2024-05-28 11:07:33 +0200

    conflicttest

--- nodes/home.server.py
+++ nodes/home.server.py
@@ -1,3 +1,5 @@
+# test
+
 {
     'hostname': '10.0.0.2',
     'groups': [
@@ -111,7 +113,7 @@
         },
         'nextcloud': {
             'hostname': 'cloud.sublimity.de',
-            'version': '28.0.1',
+            'version': '29.0.1',
             'config': {
                 'instanceid': 'oci6dw1woodz',
                 'secret': '!decrypt:encrypt$gAAAAABj96CFynVtEgsje7173zjQAcY7xQG3uyf5cxE-sJAvhyPh_KUykTKdwnExc8NTDJ8RIGUmVfgC6or5crnYaggARPIEg5-Cb0xVdEPPZ3oZ01ImLmynLu3qXT9O8kVM-H21--OKeztMRn7bySsbXdWEGtETFQ==',
