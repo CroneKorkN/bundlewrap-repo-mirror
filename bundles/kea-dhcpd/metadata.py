7bd5f41bc91ccae8716c2a9235fc0bee5e2338c5
cronekorkn <git@ckn.li>
2024-09-23 20:32:12 +0200

    dhcp from interface

--- bundles/kea-dhcpd/metadata.py
+++ bundles/kea-dhcpd/metadata.py
@@ -1,3 +1,8 @@
+from ipaddress import ip_interface
+
+hashable = repo.libs.hashable.hashable
+
+
 defaults = {
     'apt': {
         'packages': {
@@ -31,7 +36,43 @@ defaults = {
 
 
 @metadata_reactor.provides(
-
+    'kea',
 )
 def subnets(metadata):
-    pass
+    subnet4 = set()
+    interfaces = set()
+
+    for network_name, network_conf in metadata.get('network').items():
+        dhcp_server_config = network_conf.get('dhcp_server_config', None)
+
+        if dhcp_server_config:
+            subnet4.add(hashable({
+                'subnet': dhcp_server_config['subnet'],
+                'pools': [
+                    {
+                        'pool': f'{dhcp_server_config['pool_from']} - {dhcp_server_config['pool_to']}',
+                    },
+                ],
+                'option-data': [
+                    {
+                        'name': 'routers',
+                        'data': dhcp_server_config['router'],
+                    },
+                    {
+                        'name': 'domain-name-servers',
+                        'data': '10.0.10.2',
+                    },
+                ],
+            }))
+            interfaces.add(network_conf.get('interface', network_name))
+
+    return {
+        'kea': {
+            'Dhcp4': {
+                'interfaces-config': {
+                    'interfaces': interfaces,
+                },
+                'subnet4': subnet4,
+            },
+        },
+    }
