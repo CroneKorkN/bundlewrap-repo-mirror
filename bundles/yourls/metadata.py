aeb0a4fbe7b46ee85d39e240c479670eedae1c67
CroneKorkN <i@ckn.li>
2025-06-22 10:07:10 +0200

    nodes/mseibert.yourls.py: introduce

---
+++ bundles/yourls/metadata.py
@@ -0,0 +1,42 @@
+defaults = {
+    'mariadb': {
+        'databases': {
+            'yourls': {
+                'password': repo.vault.random_bytes_as_base64_for(f'{node.name} yourls DB', length=32).value,
+            },
+        },
+    },
+}
+
+
+@metadata_reactor.provides(
+    'apt/packages',
+)
+def apt(metadata):
+    php_version = metadata.get('php/version')
+
+    return {
+        'apt':{
+            'packages': {
+                f'php{php_version}-mysql': {},
+            },
+        },
+    }
+
+
+@metadata_reactor.provides(
+    'nginx/vhosts',
+)
+def nginx(metadata):
+    return {
+        'nginx': {
+            'vhosts': {
+                metadata.get('yourls/hostname'): {
+                    'content': 'yourls/vhost.conf',
+                    'context': {
+                        'php_version': metadata.get('php/version'),
+                    },
+                },
+            },
+        },
+    }
