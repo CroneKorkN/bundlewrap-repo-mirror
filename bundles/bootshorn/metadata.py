951fa6329636be34d65653ed24769d72893c7f05
CroneKorkN <i@ckn.li>
2025-07-13 10:13:23 +0200

    bootshorn better temp logging +

--- bundles/bootshorn/metadata.py
+++ bundles/bootshorn/metadata.py
@@ -25,7 +25,7 @@ defaults = {
     'systemd-timers': {
         'bootshorn-temperature': {
             'command': '/opt/bootshorn/temperature',
-            'when': '*:0/5',
+            'when': '*:0/10',
             'working_dir': '/opt/bootshorn',
             'user': 'ckn',
             'group': 'ckn',
