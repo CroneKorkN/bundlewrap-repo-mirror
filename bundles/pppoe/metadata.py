962bd06a32b603f46d02aa6f32405d29a4f68345
CroneKorkN <i@ckn.li>
2025-08-03 22:38:12 +0200

    qdisc-ppp0 partof pppoe-isp

--- bundles/pppoe/metadata.py
+++ bundles/pppoe/metadata.py
@@ -26,16 +26,23 @@ defaults = {
             'qdisc-ppp0.service': {
                 'Unit': {
                     'Description': 'setup queuing discipline for interface ppp0',
-                    'After': 'sys-devices-virtual-net-ppp0.device',
+                    'After': {
+                        'pppoe-isp.service',
+                        'sys-devices-virtual-net-ppp0.device',
+                    },
+                    'PartOf': 'pppoe-isp.service',
                     'BindsTo': 'sys-devices-virtual-net-ppp0.device',
                 },
                 'Service': {
                     'Type': 'oneshot',
-                    'ExecStart': '/sbin/tc qdisc replace root dev ppp0 cake bandwidth 30Mbit rtt 50ms diffserv4 nat egress',
+                    'ExecStart': '/sbin/tc qdisc replace root dev ppp0 cake bandwidth 37Mbit internet besteffort triple-isolate nat egress memlimit 256mb',
+                    # - no drops save
+                    # - bis 37MBit keine retries bei: iperf3 --client 49.12.184.229 -t999 -i5 --bidir
+                    #'ExecStart': '/sbin/tc qdisc replace root dev ppp0 cake bandwidth 37Mbit internet besteffort nat egress memlimit 256mb',
                     'RemainAfterExit': 'yes',
                 },
                 'Install': {
-                    'WantedBy': 'network-online.target',
+                    'WantedBy': 'multi-user.target',
                 },
             }
         },
