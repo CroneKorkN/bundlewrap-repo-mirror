7586d4ff29e3cfde50ed6b4e3b08822346c856c2
CroneKorkN <i@ckn.li>
2025-08-09 22:45:19 +0200

    remove unnecessary locales

--- bundles/locale/metadata.py
+++ bundles/locale/metadata.py
@@ -7,12 +7,7 @@ defaults = {
     'locale': {
         'default': ('en_US.UTF-8', 'UTF-8'),
         'installed': {
-            ('de_AT.UTF-8', 'UTF-8'),
-            ('de_CH.UTF-8', 'UTF-8'),
             ('de_DE.UTF-8', 'UTF-8'),
-            ('de_LU.UTF-8', 'UTF-8'),
-            ('en_CA.UTF-8', 'UTF-8'),
-            ('en_GB.UTF-8', 'UTF-8'),
             ('en_US.UTF-8', 'UTF-8'),
         },
     },
