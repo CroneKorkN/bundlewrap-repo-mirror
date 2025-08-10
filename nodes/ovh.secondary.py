2a8a30add1342151348a797ef689fcf0ff311ab7
CroneKorkN <i@ckn.li>
2025-08-10 19:22:18 +0200

    wip

--- nodes/ovh.secondary.py
+++ nodes/ovh.secondary.py
@@ -7,7 +7,7 @@
     ],
     'bundles': [
         'wireguard',
-        'left4dead2',
+        #'left4dead2',
     ],
     'metadata': {
         'id': 'd5080b1a-b310-48be-bd5a-02cfcecf0c90',
@@ -47,12 +47,19 @@
                 },
             },
         },
-        'left4dead2': {
-            'servers': {
-                'One': {
-                    'port': 27015,
-                },
+        'nftables': {
+            'input': {
+                'tcp dport 27015 accept',
+                'udp dport { 27005, 27015, 27020 } accept',
             },
         },
+
+        # 'left4dead2': {
+        #     'servers': {
+        #         'One': {
+        #             'port': 27015,
+        #         },
+        #     },
+        # },
     },
 }
