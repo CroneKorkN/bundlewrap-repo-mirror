982a27739aa95196e74c52fcc538ca88f06a67b5
CroneKorkN <i@ckn.li>
2026-01-10 11:10:56 +0100

    bundles/routeros-monitoring/items.py: dont show mib diff

--- bundles/routeros-monitoring/items.py
+++ bundles/routeros-monitoring/items.py
@@ -2,6 +2,7 @@ files = {
     # https://mikrotik.com/download/tools
     '/usr/share/snmp/mibs/MIKROTIK-MIB.txt': {
         'source': 'mikrotik.mib',
+        'content_type': 'binary',
         'mode': '0644',
         'needed_by': {
             'svc_systemd:telegraf.service',
