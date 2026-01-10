4652a42346d817cff5d511d08084e8fcfcd901c8
CroneKorkN <i@ckn.li>
2026-01-10 11:10:35 +0100

    disable zfs mirror for now

--- nodes/home.backups.py
+++ nodes/home.backups.py
@@ -12,7 +12,7 @@
         'smartctl',
         'wol-sleeper',
         'zfs',
-        'zfs-mirror',
+        #'zfs-mirror',
     ],
     'metadata': {
         'id': '9cf52515-63a1-4659-a8ec-6c3c881727e5',
